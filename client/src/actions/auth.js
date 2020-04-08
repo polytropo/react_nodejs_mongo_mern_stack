87580d01c6cfe43f7a4de945294729f011da629b
Primoz <primoz969@gmail.com>
2020-04-08 22:17:30 +0200

    adds option to create profile and to edit profile

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -68,7 +68,7 @@ export const login = (email, password) => async dispatch => {
     headers: {
       'Content-Type': 'application/json'
     }
-  }
+  };
 
   const body = JSON.stringify({ email, password });
 
--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -22,3 +22,40 @@ export const getCurrentProfile = () => async dispatch => {
     });
   }
 };
+
+// Create or update profile
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+  try {
+    // Because we are sending data we need to create header object
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    };
+
+    const res = await axios.post('http://localhost:5000/api/profile', formData, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+
+    dispatch(setAlert(edit ? 'Profile Update' : 'Profile Created', 'success'));
+
+    if (!edit) {
+      // history method has push in it
+      history.push('/dashboard');
+    }
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
